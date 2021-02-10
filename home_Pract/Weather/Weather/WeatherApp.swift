7f335597b31b8dae4f6789e4fdc96644cfda1408
Marken <27118779+Marken2808@users.noreply.github.com>
2021-02-10 16:49:11 +0000

    n

---
+++ home_Pract/Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Tuan on 10/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

84347768bb46a2623596941f104bf623b68fd9cc
Marken <27118779+Marken2808@users.noreply.github.com>
2021-05-06 10:17:55 +0100

    update on 6/5

--- home_Pract/CardWallet/CardWallet/ViewModel/CardViewModel.swift
+++ home_Pract/CardWallet/CardWallet/ViewModel/CardViewModel.swift
@@ -8,27 +8,16 @@
 import SwiftUI
 
 class CardViewModel : ObservableObject {
-    
     @Published var brands = [Card]()
-    
     init() {
         self.brands = [
-            
             Card(id: 1, name: "Morrisons", image: "Morrisons-front", expand: false),
-            
             Card(id: 2, name: "Boots", image: "Boots-front", expand: false),
-            
             Card(id: 3, name: "Tesco", image: "Tesco-front", expand: false),
-            
             Card(id: 4, name: "Iceland", image: "Iceland-front", expand: false),
-            
             Card(id: 6, name: "Nectar", image: "Nectar-front", expand: false),
-            
             Card(id: 7, name: "Holland&Barrett", image: "Holland&Barrett-front", expand: false),
-            
             Card(id: 8, name: "Superdrug", image: "Superdrug-front", expand: false),
-        
         ]
     }
-    
 }
